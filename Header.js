cf20f5c95efa02bf90675d00612a7e71ed6f2e47
Lucas Ribeiro <70024244+Lucas2003-18@users.noreply.github.com>
2022-04-25 08:44:59 -0300

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Header.css';
+
+export default ({black}) => {
+    return(
+        <header className={black ? 'black' : ''}>
+            <div className="header--logo">
+                <a href="/">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="Netflix"/>                </a>
+            </div>
+            <div className="header--user">
+                <a href="/">
+                    <img src="https://pbs.twimg.com/profile_imagens/1240119990411550720/hBEe3tdn_400x400.png" alt="Imagem de usuário"/>
+                </a>
+            </div>
+        </header>
+    );
+}
